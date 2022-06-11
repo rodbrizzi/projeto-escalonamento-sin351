9297d6f88316901dd7df874aaeba111a6feb8878
rodbrizzi <rodbrizzi@gmail.com>
2022-06-11 00:47:42 -0300

    SJF implementado

--- algorithms/SJF.h
+++ algorithms/SJF.h
@@ -21,6 +21,7 @@ void sjf_calculate_time(Process *p, int len)
 	p[0].return_time = p[0].burst;
 	p[0].turnaround_time = p[0].burst - p[0].arrive_time;
 	p[0].waiting_time = 0;
+	p[0].response_time = p[0].waiting_time;
 	
 	curr_time = p[0].burst;
 
@@ -58,6 +59,7 @@ void sjf_calculate_time(Process *p, int len)
 		}
 
 		p[min].waiting_time = curr_time - p[min].arrive_time;
+		p[min].response_time = p[min].waiting_time;
 
 		p[min].completed = TRUE;
 
@@ -137,7 +139,55 @@ void sjf_print_gantt_chart(Process *p, int len)
 
 
 void SJF(Process *p, int len){
-    printf("SJF - Implememtar e devolver no final, o tempo de espera, tempo de retorno e o tempo de resposta");
+    printf("\tExemplo do SJF:\n\n");
+
+	int i;
+	int total_waiting_time = 0;
+	int total_turnaround_time = 0;
+	int total_response_time = 0;
+
+	
+
+
+
+	process_init(p, len);
+	
+	// ordena por ordem de chegada, pois mesmo se tratando
+	// de um algoritmo que executa primeiro os processos 
+	// com menor tempo de execução, primeiramente o algoritmo
+	// deve verificar quais processos estão disponíveis no momento
+	// para serem executados.
+	merge_sort_by_arrive_time(p, 0, len);
+
+	// função que verifica a ordem de execução dos processos
+	// de acordo com o algoritmo SJF, e calcula o tempo de retorno,
+	// tempo de resposta, tempo de espera, turnaround.
+	sjf_calculate_time(p, len);
+
+	for(i = 0; i < len; i++){
+		total_waiting_time += p[i].waiting_time;
+		total_turnaround_time += p[i].turnaround_time;
+		total_response_time += p[i].response_time;
+	}
+
+	// função para ordenar de forma crescente os processos com base no tempo de retorno,
+	// OBS: Usa essa ordenação para imprimir a sequência que foram executados os processos
+	quick_sort_by_return_time(p, len);
+
+	//função que monta o gráfico de gantt
+	sjf_print_gantt_chart(p, len);
+
+	//Saídas OBS: copiado do exemplo PPS
+    //Tempo médio de espera | Tempo médio Turnaround | Tempo médio de resposta
+    printf("\n\tAverage Waiting Time     : %-2.2lf\n", (double)total_waiting_time / (double)len);
+    printf("\tAverage Turnaround Time  : %-2.2lf\n", (double)total_turnaround_time / (double)len);
+    printf("\tAverage Response Time    : %-2.2lf\n\n", (double)total_response_time / (double)len);
+
+	print_table(p, len);
+
+
+
+
 
 }
 
